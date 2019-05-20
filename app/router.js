05e3a1bbd2aee35fb4480dcf4026cd783c6a6b28
jshaows <jacky.shaows@gmail.com>
2019-05-20 02:44:53 +0200

    Fix router.js

--- app/router.js
+++ app/router.js
@@ -2,7 +2,6 @@ import Vue from 'vue';
 import Router from 'vue-router';
 
 import Home from './components/Home.vue';
-import Characters from './components/Characters.vue';
 import Game from './components/Game.vue';
 import Win from './components/Win.vue';
 import Lose from './components/Lose.vue';
@@ -17,11 +16,6 @@ const router = new Router({
       path: '/',
       name: 'home',
       component: Home
-    },
-    {
-      path: '/characters',
-      name: 'characters',
-      component: Characters
     },
     {
       path: '/game/:id',
