6312d9aeff3651d7e2b55919780c7feb51a8decc
jshaows <jacky.shaows@gmail.com>
2019-05-21 14:33:49 +0200

    Save step game

--- app/components/Home.vue
+++ app/components/Home.vue
@@ -2,15 +2,19 @@
   <div class="home">
     <audio src="/assets/audios/home.mp3" autoplay loop></audio>
     <h1 class="home__text">{{ message }}</h1>
-    <router-link class="home__link" to="/game/1">Jouer</router-link>
+    <router-link class="home__link" to="/characters">Votre héro</router-link>
+    <router-link class="home__link" :to="'/game/'+go.toString()">Jouer</router-link>
   </div>
 </template>
 
 <script>
+import stepCountService from "../services/stepCountService";
+import charactersService from "../services/charactersService";
 export default {
   data() {
     return {
-      message: "Welcome to the dungeon"
+      message: "Welcome to the dungeon",
+      go: stepCountService.value()
     };
   }
 };
